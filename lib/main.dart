5a6f5541871a0afa538136e78a35fcb182bf6eec
Chrysanthus Jumaa <jumaachrysanthus@gmail.com>
2025-11-04 11:25:38 +0300

    Utilisation of scaffold, together with the column and row widget

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+// IMPORTING THE MATERIAL DESIGN LIBRARY
+import 'package:flutter/material.dart';
+
+// DEFINE THE APPLICATION ENTRY POINT
+void main() {
+  runApp(MyApp());
+}
+
+// DEFINE THE ROOT OF THE APP
+class MyApp extends StatelessWidget {
+  // CALLING OUR CLASS CONSTRUCTOR
+  const MyApp({super.key});
+
+  // BUILD THE ROOT OF OUR APPLICATION
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+  appBar: AppBar(title: Text("Flutter Widgets")),
+  body: Center(
+    child: Column(
+  children: [
+    Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Icon(Icons.home, size: 30),
+        Icon(Icons.star, size: 30),
+        Icon(Icons.settings, size: 30),
+      ],
+    ),
+    Padding(
+      padding: EdgeInsets.all(20),
+      child: Text("Hello Flutter!"),
+    ),
+  ],
+)
+  ),
+),
+    );
+  }
+}
